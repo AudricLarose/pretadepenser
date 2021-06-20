60a00f5e76d4844a7a177c1c4e31045bcefdf0cf
Larose Audric <l.audric@gmail.com>
2021-06-20 22:55:14 +0200

    simple

--- index.py
+++ index.py
@@ -1,11 +1,18 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0)
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import numpy as np
 import dash  # (version 1.12.0) pip install dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+from dash.dependencies import Input, Output
+import pickle
+from sklearn.model_selection import train_test_split
+import json
+import pathlib
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
-
-
 
 app.layout = html.Div([
     html.P(['Entrer votre numero de clients et appuyez sur " Entrez " ']),
